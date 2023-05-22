2a9ca109898a81808ce988a09317f134a092b663
jhon anderson <apxanderson02@gmail.com>
2023-05-22 16:03:21 -0500

    diseño riesgo tabla

--- app/Views/admin/home/main.php
+++ app/Views/admin/home/main.php
@@ -9,7 +9,35 @@ require '../../layouts/admin.php';
 <!-- Contenido específico de la vista -->
 <div class="container-fluid my-3">
 
-<h4>Pagina principal</h4>
+<table class="matriz-riesgo-table">
+    <thead>
+        <tr>
+            <th></th>
+            <th>Bajo</th>
+            <th>Medio</th>
+            <th>Alto</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th>Bajo</th>
+            <td class="controlable">Controlable</td>
+            <td class="atencion">Atención</td>
+            <td class="prioridad">Prioridad</td>
+        </tr>
+        <tr>
+            <th>Medio</th>
+            <td class="atencion">Atención</td>
+            <td class="importante">Importante</td>
+            <td class="alta-prioridad">Alta prioridad</td>
+        </tr>
+        <tr>
+            <th>Alto</th>
+            <td class="prioridad">Prioridad</td>
+            <td class="alta-prioridad">Alta prioridad</td>
+            <td class="critico">Crítico</td>
+        </tr>
+    </tbody>
+</table>
 
 </div>
-
