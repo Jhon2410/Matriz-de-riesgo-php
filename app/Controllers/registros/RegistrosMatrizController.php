034ddbf5a3ca6614e48bd5d7b78bc0c96715a479
jhon anderson <apxanderson02@gmail.com>
2023-06-08 00:37:24 -0500

    db fix

--- app/Controllers/registros/RegistrosMatrizController.php
+++ app/Controllers/registros/RegistrosMatrizController.php
@@ -1,29 +1,6 @@
 <?php
-class Database
-{
-    private static $servername = "localhost";
-    private static $username = "root";
-    private static $password = "";
-    private static $database = "matriz";
-    private static $conn;
 
-    public static function connect()
-    {
-        self::$conn = new mysqli(self::$servername, self::$username, self::$password, self::$database);
-
-        if (self::$conn->connect_error) {
-            die("Conexión fallida: " . self::$conn->connect_error);
-        }
-    }
-
-    public static function getConnection()
-    {
-        if (!self::$conn) {
-            self::connect();
-        }
-        return self::$conn;
-    }
-}
+require_once './../../util/db.php';
 
 class RegistroController
 {
