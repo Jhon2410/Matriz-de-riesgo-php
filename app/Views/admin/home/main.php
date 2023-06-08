8a52bc84d5d798c4d96a7cf11f9e31b24e209805
jhon anderson <apxanderson02@gmail.com>
2023-06-08 00:16:59 -0500

    fix crear modal detalles registros

--- app/Views/admin/home/main.php
+++ app/Views/admin/home/main.php
@@ -89,7 +89,7 @@ $contador = 0;
     <?php foreach ($matriz_de_riesgo as $index => $matriz): ?>
         <?php echo $index; ?>
         <table class="matriz-riesgo-table my-3">
-      
+
             <thead>
                 <tr>
                     <th>
@@ -121,40 +121,9 @@ $contador = 0;
                 </tr>
             </tbody>
         </table>
-     
+
         <script>
-            
-            function mostrarModal(detalle) {
-                // Buscar el registro correspondiente al detalle proporcionado
-                var registro = <?php echo json_encode($matriz['registros']); ?>.find(function (registro) {
-                    return registro['riesgo'] === detalle;
-                });
 
-                if (registro) {
-                    var itemDetails = document.getElementById('itemDetails');
-                    itemDetails.innerHTML = '';
-
-                    // Crear elementos HTML para mostrar los detalles del riesgo
-                    var titulo = document.createElement('h4');
-                    titulo.textContent = detalle;
-                    itemDetails.appendChild(titulo);
-
-                    var impacto = document.createElement('p');
-                    impacto.textContent = 'Nivel de impacto: ' + registro['impacto'];
-                    itemDetails.appendChild(impacto);
-
-                    var probabilidad = document.createElement('p');
-                    probabilidad.textContent = 'Nivel de probabilidad: ' + registro['posibilidad_ocurrencia'];
-                    itemDetails.appendChild(probabilidad);
-
-                    var probabilidad = document.createElement('p');
-                    probabilidad.textContent = 'Caso de control: ' + registro['proceso_correctivo'];
-                    itemDetails.appendChild(probabilidad);
-
-                    // Mostrar el modal
-                    $('#itemModal').modal('show');
-                }
-            }
             var registros = <?php echo json_encode($matriz['registros']); ?>;
 
             if (registros && registros.length > 0 && registros[0]['id'] !== null) {
@@ -164,7 +133,7 @@ $contador = 0;
                     var clasificacion = clasificarRiesgo(nivelImpacto, nivelProbabilidad);
 
                     var celda;
-                    
+
                     switch (nivelImpacto) {
                         case 'Bajo':
                             switch (nivelProbabilidad) {
@@ -209,17 +178,13 @@ $contador = 0;
 
                     if (celda) {
                         var parrafo = document.createElement('p');
-                        parrafo.setAttribute('class', 'shadow-item');
+                        parrafo.setAttribute('class', 'shadow-item registro');
+                        parrafo.setAttribute('data-registro', JSON.stringify(registro));
                         parrafo.textContent = registro['riesgo'];
                         parrafo.setAttribute('title', clasificacion);
                         parrafo.setAttribute('data-toggle', 'tooltip');
                         parrafo.setAttribute('data-placement', 'top');
                         parrafo.setAttribute('data-html', 'true');
-
-                        // Agregar evento onclick para mostrar el modal
-                        parrafo.addEventListener('click', function () {
-                            mostrarModal(registro['riesgo']);
-                        });
 
                         celda.appendChild(parrafo);
                     }
@@ -228,5 +193,78 @@ $contador = 0;
         </script>
     <?php endforeach; ?>
 
+
+    <script>
+        function mostrarModal(registro) {
+            if (registro) {
+                var itemDetails = document.getElementById('itemDetails');
+                itemDetails.innerHTML = '';
+
+                // Crear elementos HTML para mostrar los detalles del riesgo
+                var titulo = document.createElement('h4');
+                titulo.textContent = registro.riesgo;
+                itemDetails.appendChild(titulo);
+
+                var impacto = document.createElement('p');
+                impacto.textContent = 'Nivel de impacto: ' + registro.impacto;
+                itemDetails.appendChild(impacto);
+
+                var probabilidad = document.createElement('p');
+                probabilidad.textContent = 'Nivel de probabilidad: ' + registro.posibilidad_ocurrencia;
+                itemDetails.appendChild(probabilidad);
+
+                var casoControl = document.createElement('p');
+                casoControl.textContent = 'Caso de control: ' + registro.proceso_correctivo;
+                itemDetails.appendChild(casoControl);
+
+                
+                var AfectaInfraCritica = document.createElement('p');
+                AfectaInfraCritica.textContent = 'Riesgo afecta infraestructura critica: ' + registro.afecta_infraestructura_critica;
+                itemDetails.appendChild(AfectaInfraCritica);
+
+                var fecha_identificacion = document.createElement('p');
+                fecha_identificacion.textContent = 'Fecha de identificacion: ' + registro.fecha_identificacion;
+                itemDetails.appendChild(fecha_identificacion);
+
+                var definicion_descripcion = document.createElement('p');
+                definicion_descripcion.textContent = 'Definicion descripcion: ' + registro.definicion_descripcion;
+                itemDetails.appendChild(definicion_descripcion);
+
+                var causas = document.createElement('p');
+                causas.textContent = 'Causas: ' + registro.causas;
+                itemDetails.appendChild(causas);
+
+                var activos_informacion_asociados = document.createElement('p');
+                activos_informacion_asociados.textContent = 'Activos informacion asociados: ' + registro.activos_informacion_asociados;
+                itemDetails.appendChild(activos_informacion_asociados);
+
+                var tipo_activo_vinculado = document.createElement('p');
+                tipo_activo_vinculado.textContent = 'Tipo activo vinculado: ' + registro.tipo_activo_vinculado;
+                itemDetails.appendChild(tipo_activo_vinculado);
+
+                var criticidad_activo = document.createElement('p');
+                criticidad_activo.textContent = 'Criticidad activo: ' + registro.criticidad_activo;
+                itemDetails.appendChild(criticidad_activo);
+
+                var tipo_riesgo = document.createElement('p');
+                tipo_riesgo.textContent = 'Tipo riesgo: ' + registro.tipo_riesgo;
+                itemDetails.appendChild(tipo_riesgo);
+
+                var matriz_riesgo_id = document.createElement('p');
+                matriz_riesgo_id.textContent = 'Matriz riesgo id: ' + registro.matriz_riesgo_id;
+                itemDetails.appendChild(matriz_riesgo_id);
+
+
+                // Mostrar el modal
+                $('#itemModal').modal('show');
+            }
+        }
+        var elementos = document.getElementsByClassName('registro');
+        Array.from(elementos).forEach(function (registro) {
+            registro.addEventListener('click', function (e) {
+                mostrarModal(JSON.parse(e.target.getAttribute('data-registro')));
+            });
+        });
+    </script>
 
 </div>
